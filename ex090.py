e1451fe3bc212fd0026787dffd4ee49c5e2e6ad5
kimhongmin1 <ytxis1@jbnu.ac.kr>
2020-12-09 19:42:48 +0900

    added python file

---
+++ ex090.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+#딕셔너리에 없는 문자열을 사용했다.
+icecream = {'폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+icecream['누가바']
+Traceback (most recent call last):
+  File "<pyshell#69>", line 1, in <module>
+    icecream['누가바']
+KeyError: '누가바'
+
+
+# In[ ]:
+
+
+
+
