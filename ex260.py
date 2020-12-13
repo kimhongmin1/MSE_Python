7813ed3c15b116f49b08ca7ae399c3694fa3c475
kimhongmin1 <ytxis1@jbnu.ac.kr>
2020-12-13 20:47:48 +0900

    added

---
+++ ex260.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[31]:
+
+
+class OMG : 
+    def print() :
+        print("Oh my god")
+
+>>> >>> myStock = OMG()
+>>> myStock.print()
+#myStock.print()가 OMG.print(myStock)으로 해석되기 때문에 def print()와 달라 오류가 발생한다.
+
+
+# In[ ]:
+
+
+
+
