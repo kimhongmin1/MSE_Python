f55be36b7b280cbb7eafb8e35573c899463030c1
kimhongmin1 <ytxis1@jbnu.ac.kr>
2020-12-13 17:53:25 +0900

    added

---
+++ ex170.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+result = 1
+for i in range(1,11):
+    result *=i
+print(result)
+#for문을 이용해 변수와 범위를 정하고 1부터 10까지의 곱을 구한다.
+
+
+# In[ ]:
+
+
+
+
