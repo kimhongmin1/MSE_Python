5a3742363a5339e4de65c415ae5a6fbbf6cf6359
kimhongmin1 <ytxis1@jbnu.ac.kr>
2020-12-13 20:02:22 +0900

    added

---
+++ ex230.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[30]:
+
+
+def my_print (a, b) :
+    print("왼쪽:", a)
+    print("오른쪽:", b)
+
+my_print(b=100, a=200)
+
+#def함수에 의해 정의된 함수에 의하여 왼쪽 : 200 오른쪽:100이 출력된다.
+
+
+# In[ ]:
+
+
+
+
