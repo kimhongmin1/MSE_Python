b014db842e6c212d11ca2076b54ab83dac4f6485
kimhongmin1 <ytxis1@jbnu.ac.kr>
2020-12-13 16:31:01 +0900

    added

---
+++ ex110.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+if True :
+    if False:
+        print("1")
+        print("2")
+    else:
+        print("3")
+else :
+    print("4")
+#첫번째 if문이 참이기때문에 두번째 if문으로 들어간후 두번째 if문도 참이기 때문에 3을 출력한다.
+print("5")
+#if문에서 출력한 3과 5를 함께 출력한다.
+
+
+# In[ ]:
+
+
+
+
