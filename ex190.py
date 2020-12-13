e701ac6d05be58fc422cd7f49cdbba0228bda8be
kimhongmin1 <ytxis1@jbnu.ac.kr>
2020-12-13 18:33:56 +0900

    added

---
+++ ex190.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+apart = [ [101, 102], [201, 202], [301, 302] ]
+
+for i in apart:
+    for a in i:
+        print(a, "호")
+print("-" * 5)
+#for문을 이용해 apart의 데이터중 한개를 선택한다
+#다음 for문을 이용해 선택한 데이터의 내용중 한개를 선택해 뒤에 "호"를 붙여 출력한다.
+
+
+# In[ ]:
+
+
+
+
