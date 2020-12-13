d0cc07bc066b37c7d8b3b0cde8f18da9d3df23ce
kimhongmin1 <ytxis1@jbnu.ac.kr>
2020-12-13 20:42:38 +0900

    added

---
+++ ex250.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[31]:
+
+
+import numpy
+for i in numpy.arange(0, 5, 0.1):
+    print(i)
+#numpy를 import한후 arange(시작, 끝, 공차)를 이용한다.
+
+
+# In[ ]:
+
+
+
+
