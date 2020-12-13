c56bdc4a930f0b37ca16049364470f111a79eb8c
kimhongmin1 <ytxis1@jbnu.ac.kr>
2020-12-13 19:58:59 +0900

    added

---
+++ ex220.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[29]:
+
+
+def print_max(a,b,c):
+    max_val = 0
+    if a >max_val:
+        max_val=a
+    if b>max_val:
+        max_val=b
+    if c>max_val:
+        max_val=c
+    print(max_val)
+#def함수를 이용해 함수를 정의한다.
+#if문을 이용해 a,b,c가 max_val보다 클때 max_val를 정의한다.
+
+
+# In[ ]:
+
+
+
+
